cc2970d15dcb6842420f7ed91649abdb876c4461
Paradox <p4r4d0x42@gmail.com>
2015-09-20 18:03:50 -0700

    Fresh project shell.
    Created a new MVVMLIGHT project for the Steam ACF tool. This will be
    part of a bigger project.

---
+++ Steam ACF Viewer/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Steam_ACF_Viewer.Model;
+
+namespace Steam_ACF_Viewer.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
---
+++ Steam ACF Viewer/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Steam_ACF_Viewer.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
